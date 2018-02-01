58137c11302051ad84ebe6afbc07dd6be3a29ded
Ni Tianhua <nitianhua@qq.com>
2018-02-01 23:15:41 +0800

    new nersion
    ad

--- shareDemo.php
+++ shareDemo.php
@@ -1,73 +1,114 @@
 <?php
-require_once "./jssdk.php"; //表示主机根目录下jssdk文件夹内jssdk.php文件
-$jssdk = new JSSDK("wxca70a66fc996c259", "986a21558a4bd4d742b4e8dbffc49361");//填写开发者中心你的开发者ID
+require_once "jssdk.php";
+$jssdk = new JSSDK("wxca70a66fc996c259", "986a21558a4bd4d742b4e8dbffc49361");
 $signPackage = $jssdk->GetSignPackage();
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title></title>
+  <title>abc</title>
 </head>
 <body>
-    <div>仅供分享，如有雷同纯属巧合</div> 
+  <div>测试</div>
 </body>
-<script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js"></script>    
-   <script>
- wx.config({
-   debug:  false,  //调式模式，设置为ture后会直接在网页上弹出调试信息，用于排查问题
-   appId: '<?php echo $signPackage["appId"];?>',
-   timestamp: <?php echo $signPackage["timestamp"];?>,
-   nonceStr: '<?php echo $signPackage["nonceStr"];?>',
-   signature: '<?php echo $signPackage["signature"];?>',
-   jsApiList: [  //需要使用的网页服务接口
-       'checkJsApi',  //判断当前客户端版本是否支持指定JS接口
-       'onMenuShareTimeline', //分享给好友
-       'onMenuShareAppMessage', //分享到朋友圈
-       // 'onMenuShareQQ',  //分享到QQ
-       'onMenuShareWeibo' //分享到微博
-   ]
- });
- wx.ready(function () {   //ready函数用于调用API，如果你的网页在加载后就需要自定义分享和回调功能，需要在此调用分享函数。//如果是微信游戏结束后，需要点击按钮触发得到分值后分享，这里就不需要调用API了，可以在按钮上绑定事件直接调用。因此，微信游戏由于大多需要用户先触发获取分值，此处请不要填写如下所示的分享API
-    wx.onMenuShareTimeline({
-        title: '震惊xxxxx', // 分享标题
-        link: 'http://www.nthsky.top/shareDemo', // 分享链接
-        imgUrl: './fengmian.jpeg', // 分享图标
-        success: function () { 
-            // 用户确认分享后执行的回调函数
-        },
-        cancel: function () { 
-            // 用户取消分享后执行的回调函数
-        }
+<script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js"></script>
+<script>
+  /*
+   * 注意：
+   * 1. 所有的JS接口只能在公众号绑定的域名下调用，公众号开发者需要先登录微信公众平台进入“公众号设置”的“功能设置”里填写“JS接口安全域名”。
+   * 2. 如果发现在 Android 不能分享自定义内容，请到官网下载最新的包覆盖安装，Android 自定义分享接口需升级至 6.0.2.58 版本及以上。
+   * 3. 常见问题及完整 JS-SDK 文档地址：http://mp.weixin.qq.com/wiki/7/aaa137b55fb2e0456bf8dd9148dd613f.html
+   *
+   * 开发中遇到问题详见文档“附录5-常见错误及解决办法”解决，如仍未能解决可通过以下渠道反馈：
+   * 邮箱地址：weixin-open@qq.com
+   * 邮件主题：【微信JS-SDK反馈】具体问题
+   * 邮件内容说明：用简明的语言描述问题所在，并交代清楚遇到该问题的场景，可附上截屏图片，微信团队会尽快处理你的反馈。
+   */
+  wx.config({
+    debug: false,
+    appId: '<?php echo $signPackage["appId"];?>',
+    timestamp: <?php echo $signPackage["timestamp"];?>,
+    nonceStr: '<?php echo $signPackage["nonceStr"];?>',
+    signature: '<?php echo $signPackage["signature"];?>',
+    jsApiList: [
+      // 所有要调用的 API 都要加到这个列表中
+      'error',
+      'checkJsApi',
+      'onMenuShareTimeline',
+      'onMenuShareAppMessage',
+      'onMenuShareQQ',
+      'onMenuShareWeibo',
+
+    ]
+  });
+  wx.ready(function () {
+    // 在这里调用 API
+    wx.error(function(res){
+    // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+    
     });
 
-    //分享给朋友
+    wx.checkJsApi({
+      jsApiList: ['chooseImage'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+      success: function(res) {
+      // 以键值对的形式返回，可用的api值true，不可用为false
+      // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+      }
+    });
+
+    wx.onMenuShareTimeline({
+      title: '震惊', // 分享标题
+      link: 'http://www.nthsky.top/shareDemo.php', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+      imgUrl: 'http://www.nthsky.top/fengmian.jpeg', // 分享图标
+      success: function () {
+      // 用户确认分享后执行的回调函数
+      },
+      cancel: function () {
+          // 用户取消分享后执行的回调函数
+          }
+    });
+
     wx.onMenuShareAppMessage({
-        title: '据说上了这个网站的都XXXX', // 分享标题
-        desc: 'UC新闻部欢迎你加入', // 分享描述
-        link: 'http://www.nthsky.top/shareDemo', // 分享链接
-        imgUrl: './fengmian.jpeg', // 分享图标
-        type: '', // 分享类型,music、video或link，不填默认为link
-        dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-        success: function () { 
-            // 用户确认分享后执行的回调函数
-        },
-        cancel: function () { 
-            // 用户取消分享后执行的回调函数
-        }
+      title: '震惊', // 分享标题
+      desc: '某男子XXXXXX', // 分享描述
+      link: 'http://www.nthsky.top/shareDemo.php', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+      imgUrl: 'http://www.nthsky.top/fengmian.jpeg', // 分享图标
+      type: '', // 分享类型,music、video或link，不填默认为link
+      dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+      success: function () {
+      // 用户确认分享后执行的回调函数
+      },
+      cancel: function () {
+      // 用户取消分享后执行的回调函数
+      }
+    });
+
+    wx.onMenuShareQQ({
+      title: '震惊', // 分享标题
+      desc: '某男子XXXXXX', // 分享描述
+      link: 'http://www.nthsky.top/shareDemo.php', // 分享链接
+      imgUrl: 'http://www.nthsky.top/fengmian.jpeg', // 分享图标
+      success: function () {
+      // 用户确认分享后执行的回调函数
+      },
+      cancel: function () {
+      // 用户取消分享后执行的回调函数
+      }
     });
 
     wx.onMenuShareWeibo({
-      title: '据说上了这个网站的都XXXX', // 分享标题
-      desc: 'UC新闻部欢迎你加入', // 分享描述
-      link: 'http://www.nthsky.top/shareDemo', // 分享链接
-      imgUrl: './fengmian.jpeg', // 分享图标
+      title: '震惊', // 分享标题
+      desc: '某男子XXXXXX', // 分享描述
+      link: 'http://www.nthsky.top/shareDemo.php', // 分享链接
+      imgUrl: 'http://www.nthsky.top/fengmian.jpeg', // 分享图标
       success: function () {
-// 用户确认分享后执行的回调函数
-    },
+      // 用户确认分享后执行的回调函数
+      },
       cancel: function () {
-// 用户取消分享后执行的回调函数
-    }
-      });
-  }); 
+      // 用户取消分享后执行的回调函数
+      }
+    });
+  });
 </script>
+</html>
