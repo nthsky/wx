b25fa5115cd9d52b476567b67f77129058db8bb4
Ni Tianhua <nitianhua@qq.com>
2018-02-01 22:44:40 +0800

    v2
    asd

--- shareDemo.php
+++ shareDemo.php
@@ -1,6 +1,6 @@
 <?php
-require_once "jssdk.php";
-$jssdk = new JSSDK("wxca70a66fc996c259", "986a21558a4bd4d742b4e8dbffc49361");
+require_once "../jssdk/jssdk.php"; //表示主机根目录下jssdk文件夹内jssdk.php文件
+$jssdk = new JSSDK("wxca70a66fc996c259", "986a21558a4bd4d742b4e8dbffc49361");//填写开发者中心你的开发者ID
 $signPackage = $jssdk->GetSignPackage();
 ?>
 <!DOCTYPE html>
@@ -12,40 +12,26 @@ $signPackage = $jssdk->GetSignPackage();
 <body>
     <div>仅供分享，如有雷同纯属巧合</div> 
 </body>
-<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
-<script>
-  /*
-   * 注意：
-   * 1. 所有的JS接口只能在公众号绑定的域名下调用，公众号开发者需要先登录微信公众平台进入“公众号设置”的“功能设置”里填写“JS接口安全域名”。
-   * 2. 如果发现在 Android 不能分享自定义内容，请到官网下载最新的包覆盖安装，Android 自定义分享接口需升级至 6.0.2.58 版本及以上。
-   * 3. 常见问题及完整 JS-SDK 文档地址：http://mp.weixin.qq.com/wiki/7/aaa137b55fb2e0456bf8dd9148dd613f.html
-   *
-   * 开发中遇到问题详见文档“附录5-常见错误及解决办法”解决，如仍未能解决可通过以下渠道反馈：
-   * 邮箱地址：weixin-open@qq.com
-   * 邮件主题：【微信JS-SDK反馈】具体问题
-   * 邮件内容说明：用简明的语言描述问题所在，并交代清楚遇到该问题的场景，可附上截屏图片，微信团队会尽快处理你的反馈。
-   */
-  wx.config({
-    debug: true,
-    appId: '<?php echo $signPackage["appId"];?>',
-    timestamp: <?php echo $signPackage["timestamp"];?>,
-    nonceStr: '<?php echo $signPackage["nonceStr"];?>',
-    signature: '<?php echo $signPackage["signature"];?>',
-    jsApiList: [
-      // 所有要调用的 API 都要加到这个列表中
-      'checkJsApi',  //判断当前客户端版本是否支持指定JS接口
+<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>    
+   <script>
+ wx.config({
+   debug:  false,  //调式模式，设置为ture后会直接在网页上弹出调试信息，用于排查问题
+   appId: '<?php echo $signPackage["appId"];?>',
+   timestamp: <?php echo $signPackage["timestamp"];?>,
+   nonceStr: '<?php echo $signPackage["nonceStr"];?>',
+   signature: '<?php echo $signPackage["signature"];?>',
+   jsApiList: [  //需要使用的网页服务接口
+       'checkJsApi',  //判断当前客户端版本是否支持指定JS接口
        'onMenuShareTimeline', //分享给好友
        'onMenuShareAppMessage', //分享到朋友圈
        'onMenuShareQQ',  //分享到QQ
-       'onMenuShareWeibo' //分享到微博<br/>
-    ]
-  });
-  wx.ready(function () {
-    // 在这里调用 API
-    //分享朋友圈
+       'onMenuShareWeibo' //分享到微博
+   ]
+ });
+ wx.ready(function () {   //ready函数用于调用API，如果你的网页在加载后就需要自定义分享和回调功能，需要在此调用分享函数。//如果是微信游戏结束后，需要点击按钮触发得到分值后分享，这里就不需要调用API了，可以在按钮上绑定事件直接调用。因此，微信游戏由于大多需要用户先触发获取分值，此处请不要填写如下所示的分享API
     wx.onMenuShareTimeline({
         title: '震惊xxxxx', // 分享标题
-        link: 'http://www.nthsky.top', // 分享链接
+        link: 'http://www.nthsky.top/shareDemo', // 分享链接
         imgUrl: './fengmian.jpeg', // 分享图标
         success: function () { 
             // 用户确认分享后执行的回调函数
@@ -59,7 +45,7 @@ $signPackage = $jssdk->GetSignPackage();
     wx.onMenuShareAppMessage({
         title: '据说上了这个网站的都XXXX', // 分享标题
         desc: 'UC新闻部欢迎你加入', // 分享描述
-        link: 'http://www.nthsky.top', // 分享链接
+        link: 'http://www.nthsky.top/shareDemo', // 分享链接
         imgUrl: './fengmian.jpeg', // 分享图标
         type: '', // 分享类型,music、video或link，不填默认为link
         dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
@@ -70,6 +56,8 @@ $signPackage = $jssdk->GetSignPackage();
             // 用户取消分享后执行的回调函数
         }
     });
-  });
+}); 
+wx.error(function (res) {
+ alert(res.errMsg);  //打印错误消息。及把 debug:false,设置为debug:ture就可以直接在网页上看到弹出的错误提示
+});
 </script>
-</html>
